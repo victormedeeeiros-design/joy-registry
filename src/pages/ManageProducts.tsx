d410cd814080118535b7ac10f37aba69681f3c22
victormedeeeiros-design <victormedeeeiros@gmail.com>
2025-09-30 01:53:26 +0000

    feat: ajuste_proporcao

--- src/pages/ManageProducts.tsx
+++ src/pages/ManageProducts.tsx
@@ -353,7 +353,7 @@ const ManageProducts = () => {
                       <img 
                         src={imageUrl} 
                         alt={product.name}
-                        className="w-full h-full object-cover"
+                        className="w-full h-full object-contain bg-white"
                         onError={(e) => {
                           const target = e.target as HTMLImageElement;
                           target.style.display = 'none';
